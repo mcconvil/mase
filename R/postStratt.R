97d9a89b1b84cd95aed120bb884f61b39312396b
joshyam-k <yamamojo@reed.edu>
2023-08-09 14:02:48 -0500

    removing magrittr dependency

--- R/postStratt.R
+++ R/postStratt.R
@@ -21,10 +21,10 @@ postStratt <- function(data, xpoptab, indices){
   #Estimator
   #Compute estimator
   dat <- data.frame(x=xsample, pi=pis,y=y)
-  tab <- dat %>%
-    group_by(x) %>%
-    summarize(poptotal_h = y%*%pi^(-1),N_h_hats = sum(pi^(-1)), var_h = var(y)) %>%
-    inner_join(xpoptab, by=c("x")) %>%
+  tab <- dat |>
+    group_by(x) |>
+    summarize(poptotal_h = y%*%pi^(-1),N_h_hats = sum(pi^(-1)), var_h = var(y)) |>
+    inner_join(xpoptab, by=c("x")) |>
     mutate(ps_h = N_h/N_h_hats, strat_pop_total = ps_h*poptotal_h, strat_pop_mean = strat_pop_total/N_h)
   
   
